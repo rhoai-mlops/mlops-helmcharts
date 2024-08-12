cca402e1bbadae65d33d477d9cd8b77f035c409a
RHRolun <rlundber@redhat.com>
2024-08-12 16:50:50 +0200

    Data pipeline working

--- bootstrap/feature_store/data_pipeline.py
+++ bootstrap/feature_store/data_pipeline.py
@@ -17,17 +17,18 @@ def extract_data(
     import psycopg2
     import pandas as pd
     import os
-    
+
     conn = psycopg2.connect(
         host=source_con_details['host'],
-        database=os.getenv("DATABASE-NAME"),
-        user=os.getenv("DATABASE-USER"),
-        password=os.getenv("DATABASE-PASSWORD"),
+        database=os.environ["database_name"],
+        user=os.environ["database_user"],
+        password=os.environ["database_password"],
     )
     query = f"SELECT * FROM {source_con_details['table']}"
     df = pd.read_sql_query(query, conn)
     conn.close()
-    df.to_csv(data, index=False)
+    data.path += ".csv"
+    df.to_csv(data.path, index=False)
 
 @component(packages_to_install=["pandas"])
 def transform_data(
@@ -36,92 +37,100 @@ def transform_data(
 ):
     import pandas as pd
     
-    df = pd.read_csv(input_data)
+    df = pd.read_csv(input_data.path)
     df.columns = map(str.lower, df.columns)
-    df.to_csv(output_data, index=False)
+    
+    output_data.path += ".csv"
+    df.to_csv(output_data.path, index=False)
 
-@component(packages_to_install=["psycopg2", "pandas"])
+@component(packages_to_install=["psycopg2","sqlalchemy==1.4.46", "pandas"])
 def load_data(
     target_con_details: dict,
     data: Input[Dataset]
 ):
-    import psycopg2
+    # import psycopg2
+    from sqlalchemy import create_engine
     import pandas as pd
     import os
     
-    conn = psycopg2.connect(
-        host=target_con_details['host'],
-        database=os.getenv("DATABASE-NAME"),
-        user=os.getenv("DATABASE-USER"),
-        password=os.getenv("DATABASE-PASSWORD"),
-    )
-    df = pd.read_csv(data)
-    df.to_sql(target_con_details['table'], conn, if_exists='replace', index=False)
-    conn.close()
+    # conn = psycopg2.connect(
+    #     host=target_con_details['host'],
+    #     database=os.environ["database_name"],
+    #     user=os.environ["database_user"],
+    #     password=os.environ["database_password"],
+    # )
+    engine = create_engine(f'postgresql://{os.environ["database_user"]}:{os.environ["database_password"]}@{target_con_details["host"]}:5432/{os.environ["database_name"]}')
+    df = pd.read_csv(data.path)
+    df.to_sql(target_con_details['table'], engine, if_exists='replace', index=False)
+    # conn.close()
 
 
 @dsl.pipeline(
   name='ETL Pipeline',
   description='Moves and transforms data from transactions data storage (postgresql) to feast storage (postgresql).'
 )
-def fraud_training_pipeline(source_con_details: dict, target_con_details: dict):
+def etl_pipeline(source_con_details: dict, target_con_details: dict):
     extract_task = extract_data(source_con_details=source_con_details)
     kubernetes.use_secret_as_env(extract_task,
                                  secret_name="transactionsdb-info",
-                                 secret_key_to_env={'database-name': 'DATABASE-NAME',
-                                                    'database-user': 'DATABASE-USER',
-                                                    'database-password': 'DATABASE-PASSWORD',
+                                 secret_key_to_env={'database-name': 'database_name',
+                                                    'database-user': 'database_user',
+                                                    'database-password': 'database_password',
                                                     })
     transform_task = transform_data(input_data=extract_task.outputs["data"])
-    load_task = load_data(target_con_details=target_con_details, 
+    load_task = load_data(target_con_details=target_con_details,
                           data=transform_task.outputs["output_data"])
     kubernetes.use_secret_as_env(load_task,
                                  secret_name="feast",
-                                 secret_key_to_env={'database-name': 'DATABASE-NAME',
-                                                    'database-user': 'DATABASE-USER',
-                                                    'database-password': 'DATABASE-PASSWORD',
+                                 secret_key_to_env={'database-name': 'database_name',
+                                                    'database-user': 'database_user',
+                                                    'database-password': 'database_password',
                                                     })
     
     
 
 if __name__ == '__main__':
-    metadata = {
-        "source_con_details": {
-            "host": "transactionsdb.mlops-transactionsdb.svc.cluster.local",
-            "table": "transactions.transactions",
-        },
-        "target_con_details": {
-            "host": "feast.mlops-feature-store.svc.cluster.local",
-            "table": "feast.feast",
+    COMPILE=False
+    if COMPILE:
+        kfp.compiler.Compiler().compile(etl_pipeline, 'transactiondb-feast-etl.yaml')
+    else:
+        metadata = {
+            "source_con_details": {
+                "host": "transactionsdb.mlops-transactionsdb.svc.cluster.local",
+                "table": "transactions.transactions",
+            },
+            "target_con_details": {
+                "host": "feast.mlops-feature-store.svc.cluster.local",
+                "table": "feast",
+            }
         }
-    }
-        
-    namespace_file_path =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-    with open(namespace_file_path, 'r') as namespace_file:
-        namespace = namespace_file.read()
 
-    kubeflow_endpoint =\
-        f'https://ds-pipeline-dspa.{namespace}.svc:8443'
+        namespace_file_path =\
+            '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+        with open(namespace_file_path, 'r') as namespace_file:
+            namespace = namespace_file.read()
 
-    sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
-    with open(sa_token_file_path, 'r') as token_file:
-        bearer_token = token_file.read()
+        kubeflow_endpoint =\
+            f'https://ds-pipeline-dspa.{namespace}.svc:8443'
 
-    ssl_ca_cert =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
+        sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
+        with open(sa_token_file_path, 'r') as token_file:
+            bearer_token = token_file.read()
 
-    print(f'Connecting to Data Science Pipelines: {kubeflow_endpoint}')
-    client = kfp.Client(
-        host=kubeflow_endpoint,
-        existing_token=bearer_token,
-        ssl_ca_cert=ssl_ca_cert
-    )
+        ssl_ca_cert =\
+            '/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
 
-    client.create_run_from_pipeline_func(
-        fraud_training_pipeline,
-        arguments=metadata,
-        experiment_name="fraud-training",
-        namespace="mlops-dev-zone",
-        enable_caching=True
-    )
+        print(f'Connecting to Data Science Pipelines: {kubeflow_endpoint}')
+        client = kfp.Client(
+            host=kubeflow_endpoint,
+            existing_token=bearer_token,
+            ssl_ca_cert=ssl_ca_cert
+        )
+
+        client.create_run_from_pipeline_func(
+            etl_pipeline,
+            arguments=metadata,
+            experiment_name="transactiondb-feast-etl",
+            namespace="mlops-feature-store",
+            enable_caching=True
+        )
